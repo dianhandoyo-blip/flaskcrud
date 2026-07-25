8c2dbee926029703a1c5e6b2b4d249e0311d430d
dianhandoyo-blip <dianhandoyo@students.amikom.ac.id>
2026-07-25 07:45:14 +0700

    Fix table name

--- db_kuliah.sql
+++ db_kuliah.sql
@@ -1,61 +1,103 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Waktu pembuatan: 02 Jul 2026 pada 11.49
--- Versi server: 10.4.32-MariaDB
--- Versi PHP: 8.2.12
+from flask import Flask, render_template, request, redirect, url_for
+from mysql import connector
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
+app = Flask(__name__)
+
+db = connector.connect(
+    host="wf01-b.h.filess.io",
+    port=3306,
+    user="db_akademik_processhas",
+    passwd="0f89d9f5f67b3e3ebe6e617310683a030033ba19",
+    database="db_akademik_processhas"
+)
+
+if db.is_connected():
+    print("Berhasil Terhubung ke Database")
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+# =========================
+# READ
+# =========================
+@app.route('/')
+def halaman_awal():
+    cur = db.cursor()
+    cur.execute("SELECT * FROM tbl_mahasiswa")
+    res = cur.fetchall()
+    cur.close()
+    return render_template('index.html', hasil=res)
 
---
--- Database: `db_kuliah`
---
 
--- --------------------------------------------------------
+# =========================
+# FORM TAMBAH
+# =========================
+@app.route('/tambah/')
+def tambah_data():
+    return render_template('tambah.html')
 
---
--- Struktur dari tabel `tbl_mahasiswa`
---
 
-CREATE TABLE `tbl_mahasiswa` (
-  `nim` varchar(10) NOT NULL,
-  `nama` varchar(25) NOT NULL,
-  `asal` varchar(15) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+# =========================
+# PROSES TAMBAH
+# =========================
+@app.route('/proses_tambah/', methods=['POST'])
+def proses_tambah():
+    nim = request.form['nim']
+    nama = request.form['nama']
+    asal = request.form['asal']
 
---
--- Dumping data untuk tabel `tbl_mahasiswa`
---
+    cur = db.cursor()
+    sql = "INSERT INTO tbl_mahasiswa (nim, nama, asal) VALUES (%s, %s, %s)"
+    cur.execute(sql, (nim, nama, asal))
+    db.commit()
+    cur.close()
 
-INSERT INTO `tbl_mahasiswa` (`nim`, `nama`, `asal`) VALUES
-('18.83.1233', 'Salsabila', 'Bantul'),
-('18.83.1234', 'Rahmadi', 'Sleman'),
-('18.83.1235', 'Sukarwo', 'Kulon Progo'),
-('18.83.1236', 'Pambudi', 'Kulon Progo'),
-('18.83.1237', 'Putri Amalia', 'Yogyakarta'),
-('24.83.1128', 'Dian Fadila Afla W H', 'Ambon');
+    return redirect(url_for('halaman_awal'))
 
---
--- Indexes for dumped tables
---
 
---
--- Indeks untuk tabel `tbl_mahasiswa`
---
-ALTER TABLE `tbl_mahasiswa`
-  ADD PRIMARY KEY (`nim`);
-COMMIT;
+# =========================
+# FORM UBAH
+# =========================
+@app.route('/ubah/<nim>')
+def ubah_data(nim):
+    cur = db.cursor()
+    cur.execute("SELECT * FROM tbl_mahasiswa WHERE nim=%s", (nim,))
+    res = cur.fetchone()
+    cur.close()
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+    return render_template('ubah.html', hasil=res)
+
+
+# =========================
+# PROSES UBAH
+# =========================
+@app.route('/proses_ubah/', methods=['POST'])
+def proses_ubah():
+    nim_lama = request.form['nim_ori']
+    nim = request.form['nim']
+    nama = request.form['nama']
+    asal = request.form['asal']
+
+    cur = db.cursor()
+    sql = "UPDATE tbl_mahasiswa SET nim=%s, nama=%s, asal=%s WHERE nim=%s"
+    value = (nim, nama, asal, nim_lama)
+    cur.execute(sql, value)
+    db.commit()
+    cur.close()
+
+    return redirect(url_for('halaman_awal'))
+
+
+# =========================
+# HAPUS DATA
+# =========================
+@app.route('/hapus/<nim>')
+def hapus_data(nim):
+    cur = db.cursor()
+    cur.execute("DELETE FROM tbl_mahasiswa WHERE nim=%s", (nim,))
+    db.commit()
+    cur.close()
+
+    return redirect(url_for('halaman_awal'))
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
